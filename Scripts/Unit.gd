0371e2146dacd6a3f37397a021af7197538400c5
TheJohnDarksoul <156362113+TheJohnDarksoul@users.noreply.github.com>
2026-01-30 20:28:11 -0500

    Added a variable to control unit army

--- Scripts/Unit.gd
+++ Scripts/Unit.gd
@@ -1,7 +1,11 @@
 class_name Unit
 extends Node2D
 
-@export var UnitName:String
+enum Armies {PLAYER_ARMY, COMPUTER_ARMY}
+
+@export var UnitName:String = "Generic Goon"
+
+@export var army:Armies = Armies.COMPUTER_ARMY
 
 @export_category("Stats")
 @export var maxHp:int = 10
@@ -11,7 +15,14 @@ extends Node2D
 @export var Con:int = 0
 @export var Agi:int = 0
 
-static var modifierTable:Array[int] = []
+var currentHp:int
+
+#Don't know how to make a static const. Don't touch this at runtime
+static var modifierTable:Array[int] = [-5, -5, -4, -4, -3, -3, -2, -2,
+-1, -1, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5]
+
+func _init() -> void:
+	currentHp = maxHp
 
 @warning_ignore("unused_parameter")
 func _process(delta: float) -> void:
