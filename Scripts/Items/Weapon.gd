3a8215b11f3e672889667911d8a9c298e6b37a0f
TheJohnDarksoul <156362113+TheJohnDarksoul@users.noreply.github.com>
2026-01-31 15:04:37 -0500

    Added classes and a global script

--- Scripts/Items/Weapon.gd
+++ Scripts/Items/Weapon.gd
@@ -3,7 +3,7 @@ class_name Weapon extends Item
 ##Amount of damage this weapon deals
 @export var damage:int
 ##Category of weapon
-@export_enum("SWORD", "GUN", "TOME", "KNUCKLES") var weaponType:int
+@export var weaponType:globs.weaponTypes
 ##Base accuracy of the weapon
 @export var accuracy:int
 ##Tiles away from the unit that can be attacked
---
+++ Scripts/UnitClass.gd
@@ -0,0 +1,6 @@
+class_name UnitClass extends Resource
+
+@export var name:String
+@export var movement:int
+
+@export var weaponTypes:Array[globs.weaponTypes]
