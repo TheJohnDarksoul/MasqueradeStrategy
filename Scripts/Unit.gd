65a70757d37a68df0379302d88e5f3f90dc9f71d
TheJohnDarksoul <156362113+TheJohnDarksoul@users.noreply.github.com>
2026-01-30 19:49:06 -0500

    Added defaults and blank modifier table

--- Scripts/Unit.gd
+++ Scripts/Unit.gd
@@ -3,9 +3,20 @@ extends Node2D
 
 @export var UnitName:String
 
-@export var Hp:int
-@export var Str:int
-@export var Dex:int
-@export var Mag:int
-@export var Con:int
-@export var Agi:int
+@export_category("Stats")
+@export var maxHp:int = 10
+@export var Str:int = 0
+@export var Dex:int = 0
+@export var Mag:int = 0
+@export var Con:int = 0
+@export var Agi:int = 0
+
+static var modifierTable:Array[int] = []
+
+@warning_ignore("unused_parameter")
+func _process(delta: float) -> void:
+	pass
+
+@warning_ignore("unused_parameter")
+func _physics_process(delta: float) -> void:
+	pass
