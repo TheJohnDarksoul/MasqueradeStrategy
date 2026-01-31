6681d0ed84aba16f19b2ac436b26d435ed6d2776
TheJohnDarksoul <156362113+TheJohnDarksoul@users.noreply.github.com>
2026-01-30 19:21:57 -0500

    Added unit node

---
+++ Scripts/Unit.gd
@@ -0,0 +1,11 @@
+class_name Unit
+extends Node2D
+
+@export var UnitName:String
+
+@export var Hp:int
+@export var Str:int
+@export var Dex:int
+@export var Mag:int
+@export var Con:int
+@export var Agi:int
