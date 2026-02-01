92797d675d40ac75e4e208a5efe360aab6b28873
Gage Koudelka <124416290+Gageinator@users.noreply.github.com>
2026-02-01 06:05:44 -0500

    Added music to the main menu amongst other changes
    Added main menu music, working volume slider for master volume, and created a build of the project confirming that the Fullscreen button works.

--- UI/items_menu.gd
+++ UI/items_menu.gd
@@ -1,11 +1,5 @@
 extends Control
 
 @onready var Item_Menu = $"."
-@onready var Cur_Hp = $HP/CurrentHP
 func _on_button_pressed() -> void:
 	Item_Menu.visible = !Item_Menu.visible
-	
-	if(Item_Menu.visible == true):
-		#Cur_Hp.text = 
-		pass
-		
